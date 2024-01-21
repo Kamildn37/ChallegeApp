4e498d64c84fe4e044b34c92b9680fcd5ec27995
Kamildn37 <dynarkam@o2.pl>
2024-01-21 18:09:29 +0100

    D4 praca domowa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Kamil!");
+﻿var name = "Ewa";
+var sex = "female";
+var age = 30;
+
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat.");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa,lat 30.");
+}
+else if (sex == "male" && age <18)
+{
+    Console.WriteLine("Niepełnotetni mężczyzna.");
+}
