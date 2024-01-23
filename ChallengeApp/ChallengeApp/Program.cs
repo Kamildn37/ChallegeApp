61017758de5e2099380b7a4bbea269f6016bb9d9
Kamildn37 <dynarkam@o2.pl>
2024-01-23 18:06:24 +0100

    Praca domowa D5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,58 @@
-﻿var name = "Ewa";
-var sex = "female";
-var age = 30;
+﻿int number = 9137;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-if (sex == "female" && age < 30)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30lat.");
-}
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa,lat 30.");
-}
-else if (sex == "male" && age <18)
-{
-    Console.WriteLine("Niepełnotetni mężczyzna.");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
